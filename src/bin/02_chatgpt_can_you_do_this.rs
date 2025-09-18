2d6f324fc2c34cf03c2410cb34cf86ed2cc7baaa
Milan Sedivy <milan.sedivy@hotmail.com>
2025-09-18 21:16:33 +0300

    Move other chapters

--- src/bin/02_chatgpt_can_you_do_this.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("Hey ChatGPT can you do this!?");
-}
---
+++ src/bin/04_chatgpt_can_you_do_this.rs
@@ -0,0 +1,36 @@
+// Hey ChatGPT can you do this?
+
+// I disabled ChatGPT from gathering my data
+// This way it will never learn from its mistakes
+// the only issue is, it tried to convince me from the start 
+// Back to slides - for a while.
+
+fn main() {
+    println!("Hey ChatGPT can you do this!?");
+    // This compiles and is absolutely valid...
+    {{{{5;}()}()}()}();
+    
+    match { {{{{5;}()}()}()}() } {
+        () => println!("Huzzah?")
+    }
+    // This one won't work
+    //match /*{*/ {{{{5;}()}()}()}() /*}*/ {
+    //         () => println!("Huzzah?")
+    //     }
+    
+    // Why?
+    // ChatGPT wrongly assumes it's a function ... after all why wouldn't it
+    // But Rust parses {}() as a BlockExpression that returns () and a () unit type
+    // However notice that ()() doesn't compile, this is because it's not 2 unit types
+    // This is however
+    ;()
+    // And so is this
+    ;;;;;;;  ;;;;;;;  ;     ;  ;;;;;;;  ;;;;;;;  ;;;;;;;  ;        ;;;;;;;  ;     ;
+    ;        ;        ;;   ;;    ;      ;        ;     ;  ;        ;     ;  ;;    ;
+    ;        ;        ; ; ; ;    ;      ;        ;     ;  ;        ;     ;  ; ;   ;
+    ;;;;;;;  ;;;;;;;  ;  ;  ;    ;      ;        ;     ;  ;        ;     ;  ;  ;  ;
+          ;  ;        ;     ;    ;      ;        ;     ;  ;        ;     ;  ;   ; ;
+          ;  ;        ;     ;    ;      ;        ;     ;  ;        ;     ;  ;    ;;
+    ;;;;;;;  ;;;;;;;  ;     ;  ;;;;;;;  ;;;;;;;  ;;;;;;;  ;;;;;;;; ;;;;;;;  ;     ;
+}
+
