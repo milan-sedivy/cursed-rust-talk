76c178f90cce191b907d9312e0abd7b838ba7dea
Milan Sedivy <milan.sedivy@hotmail.com>
2025-09-18 21:16:15 +0300

    add chap 03 wait a minute

---
+++ src/bin/03_wait_a_minute_what.rs
@@ -0,0 +1,71 @@
+
+// 03 - Wait a minute, what?
+
+// From the rust reference book:
+/*
+    CallExpression → Expression ( CallParams? )
+
+    Expression →
+        ExpressionWithoutBlock
+        | ExpressionWithBlock
+
+    ExpressionWithoutBlock →
+        OuterAttribute*
+        (
+           LiteralExpression
+         | PathExpression
+         | OperatorExpression
+         | GroupedExpression
+         | ArrayExpression
+         | AwaitExpression
+         | IndexExpression
+         | TupleExpression
+         | TupleIndexingExpression
+         | StructExpression
+         | CallExpression
+         | MethodCallExpression
+         | FieldExpression
+         | ClosureExpression
+         | AsyncBlockExpression
+         | ContinueExpression
+         | BreakExpression
+         | RangeExpression
+         | ReturnExpression
+         | UnderscoreExpression
+         | MacroInvocation
+        )
+
+    ExpressionWithBlock →
+        OuterAttribute*
+        (
+           BlockExpression
+         | ConstBlockExpression
+         | UnsafeBlockExpression
+         | LoopExpression
+         | IfExpression
+         | MatchExpression
+        )
+ */
+
+fn wait_a_minute_what() {
+    {}(); // <-- explained in next example file ;)
+    (|x: i32| println!("{}", x))(5);
+    ({
+        let x= |x: i32| println!("{x}");
+        x
+    })
+        (5);
+    // This won't work:
+    // (if true {|x| println!("x: {}", x)})(5);
+    // But this will:
+    (if true {|x| println!("x: {}", x)} else {return})(5);
+}
+
+
+fn main() {
+    println!("Wait a minute what!");
+
+
+}
+
+// 03 - Wait a minute, what?
