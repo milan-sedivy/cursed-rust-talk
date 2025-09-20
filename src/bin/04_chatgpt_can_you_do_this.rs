d8242f2cba4ff5cb69a2cfbf05128f9ac5b786d7
Milan Sedivy <milan.sedivy@hotmail.com>
2025-09-20 12:31:44 +0300

    fix typos

--- src/bin/04_chatgpt_can_you_do_this.rs
+++ src/bin/04_chatgpt_can_you_do_this.rs
@@ -2,14 +2,14 @@
 
 // I disabled ChatGPT from gathering my data
 // This way it will never learn from its mistakes
-// the only issue is, it tried to convince me from the start 
+// the only issue is, it tried to convince me from the start
 // Back to slides - for a while.
 
 fn main() {
     println!("Hey ChatGPT can you do this!?");
     // This compiles and is absolutely valid...
     {{{{5;}()}()}()}();
-    
+
     match { {{{{5;}()}()}()}() } {
         () => println!("Huzzah?")
     }
@@ -17,15 +17,15 @@ fn main() {
     //match /*{*/ {{{{5;}()}()}()}() /*}*/ {
     //         () => println!("Huzzah?")
     //     }
-    
+
     // Why?
     // ChatGPT wrongly assumes it's a function ... after all why wouldn't it
     // But Rust parses {}() as a BlockExpression that returns () and a () unit type
     // However notice that ()() doesn't compile, this is because it's not 2 unit types
     // it's treated as something that should be callable but instead is invalid syntax
-    // This is however fine and is 
+    // This is however fine
     ;();();()
-    // And because of semicolons work, so is this
+    // And because of how semicolons work, so is this
     ;;;;;;;  ;;;;;;;  ;     ;  ;;;;;;;  ;;;;;;;  ;;;;;;;  ;        ;;;;;;;  ;     ;
     ;        ;        ;;   ;;    ;      ;        ;     ;  ;        ;     ;  ;;    ;
     ;        ;        ; ; ; ;    ;      ;        ;     ;  ;        ;     ;  ; ;   ;
